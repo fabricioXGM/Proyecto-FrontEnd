37c8de73df0ea79d2c0361fb163cb1de968047b1
fabricioXGM <fabricioxgalarzamirada@hotmail.com>
2023-06-20 16:59:27 -0500

    Iniciado

---
+++ main.js
@@ -0,0 +1,6 @@
+desplegable = document.querySelector(".desplegable");
+
+desplegable.onclick = function(){
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("activar");
+} 
